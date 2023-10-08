33daad38bef0753d5331a70a245a79c4609ae55f
Gavin Cooper <gavinjcooper@outlook.com>
2023-10-08 11:59:04 +0100

    First test

--- src/Unit-1/WinTail.Tests/UnitTest1.cs
+++ src/Unit-1/WinTail.Tests/UnitTest1.cs
@@ -1,18 +1,65 @@
 using Microsoft.VisualStudio.TestPlatform.TestHost;
+using System.Text;
 
 namespace WinTail.Tests
 {
     [UsesVerify]
-    public class UnitTest1
+    public class UnitTest1 : IConsoleReader
     {
+        private ManualResetEventSlim handle = new ManualResetEventSlim(false);
+        private string value = "testing";
+        
+        public string ReadLine()
+        {
+            handle.Wait();
+            handle.Reset();
+
+            return value;
+        }
+
+        private void SetValue(string value)
+        {
+            this.value = value;
+            handle.Set();
+        }
+
         [Fact]
-        public void Test1()
+        public async Task Test1()
         {
             var outputPrinter = new ConsolePrinter();
-            var sut = new Application(outputPrinter);
+            var sut = new Application(outputPrinter, this);
 
-            sut.Run(new string[] { });
+            var actTask = Task.Run(() => sut.Run(new string[] { }));
+            SetValue("test");
 
+            await Task.Delay(1000);
+            SetValue("exit");
 
+            await actTask;
+
+            await Verifier.Verify(outputPrinter.ToString());
         }
+    }
+
+    internal class ConsolePrinter : IConsoleWriter
+    {
+        StringBuilder sb = new StringBuilder(); 
+
+        public ConsoleColor ForegroundColor { get; set; }
+
+        public void ResetColor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteLine(object v)
+        {
+            sb.AppendLine(v.ToString());
+        }
+
+        public override string ToString()
+        {
+            return sb.ToString();
+        }
+    }
 }

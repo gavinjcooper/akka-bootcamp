ea374847aaf44e644ad434158bd0713654cf13ac
Gavin Cooper <gavin.cooper@electron.net>
2023-10-13 08:33:11 +0100

    lesson 4

--- src/Unit-1/DoThis/ConsoleReaderActor.cs
+++ src/Unit-1/DoThis/ConsoleReaderActor.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Security.Cryptography;
 using Akka.Actor;
 
 namespace WinTail
@@ -12,7 +11,7 @@ namespace WinTail
     {
         public const string StartCommand = "start";
         public const string ExitCommand = "exit";
-        private IActorRef _validationActor;
+        private readonly IActorRef _validationActor;
 
         public ConsoleReaderActor(IActorRef validationActor)
         {
@@ -32,9 +31,7 @@ namespace WinTail
         #region Internal methods
         private void DoPrintInstructions()
         {
-            Console.WriteLine("Write whatever you want into the console!");
-            Console.WriteLine("Some entries will pass validation, and some won't...\n\n");
-            Console.WriteLine("Type 'exit' to quit this application at any time.\n");
+            Console.WriteLine("Please provide the URI of a log file on disk.\n");
         }
 
         /// <summary>
@@ -44,30 +41,17 @@ namespace WinTail
         private void GetAndValidateInput()
         {
             var message = Console.ReadLine();
-            if (string.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
+            if (!string.IsNullOrEmpty(message) && String.Equals(message, ExitCommand, StringComparison.OrdinalIgnoreCase))
             {
-                // if user typed ExitCommand, shut down the entire actor
-                // system (allows the process to exit)
+                // if user typed ExitCommand, shut down the entire actor system (allows the process to exit)
                 Context.System.Terminate();
                 return;
             }
 
-            // otherwise, just hand message off to validation actor
-            // (by telling its actor ref)
+            // otherwise, just hand message off to validation actor (by telling its actor ref)
             _validationActor.Tell(message);
         }
 
-        /// <summary>
-        /// Validates <see cref="message"/>.
-        /// Currently says messages are valid if contain even number of characters.
-        /// </summary>
-        /// <param name="message"></param>
-        /// <returns></returns>
-        private static bool IsValid(string message)
-        {
-            var valid = message.Length % 2 == 0;
-            return valid;
-        }
         #endregion
     }
 }
